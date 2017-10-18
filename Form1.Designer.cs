3d4581ba745b2f11cfe064829292c396ffeef62c
HugoCezar <31222011+HugoCezar@users.noreply.github.com>
2017-10-18 16:28:39 -0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Exercicio_1110
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.lblParse = new System.Windows.Forms.Label();
+            this.lblTryParse = new System.Windows.Forms.Label();
+            this.txtLimpar = new System.Windows.Forms.Button();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(45, 39);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(90, 13);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Insira um número:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(153, 39);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(145, 20);
+            this.txtNumero.TabIndex = 1;
+            this.txtNumero.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNumero_keydown);
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(175, 150);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(79, 31);
+            this.btnConverter.TabIndex = 2;
+            this.btnConverter.Text = "Clique Aqui!";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // lblParse
+            // 
+            this.lblParse.AutoSize = true;
+            this.lblParse.Location = new System.Drawing.Point(91, 216);
+            this.lblParse.Name = "lblParse";
+            this.lblParse.Size = new System.Drawing.Size(44, 13);
+            this.lblParse.TabIndex = 3;
+            this.lblParse.Text = "\"Parse\"";
+            // 
+            // lblTryParse
+            // 
+            this.lblTryParse.AutoSize = true;
+            this.lblTryParse.Location = new System.Drawing.Point(272, 216);
+            this.lblTryParse.Name = "lblTryParse";
+            this.lblTryParse.Size = new System.Drawing.Size(59, 13);
+            this.lblTryParse.TabIndex = 4;
+            this.lblTryParse.Text = "\"TryParse\"";
+            // 
+            // txtLimpar
+            // 
+            this.txtLimpar.Location = new System.Drawing.Point(330, 39);
+            this.txtLimpar.Name = "txtLimpar";
+            this.txtLimpar.Size = new System.Drawing.Size(75, 20);
+            this.txtLimpar.TabIndex = 5;
+            this.txtLimpar.Text = "Limpar";
+            this.txtLimpar.UseVisualStyleBackColor = true;
+            this.txtLimpar.Click += new System.EventHandler(this.txtLimpar_Click);
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Location = new System.Drawing.Point(48, 79);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(90, 13);
+            this.lblNumero2.TabIndex = 6;
+            this.lblNumero2.Text = "Insira um número:";
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(153, 79);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(145, 20);
+            this.txtNumero2.TabIndex = 7;
+            this.txtNumero2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNumero2_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 276);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.txtLimpar);
+            this.Controls.Add(this.lblTryParse);
+            this.Controls.Add(this.lblParse);
+            this.Controls.Add(this.btnConverter);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblNumero);
+            this.Name = "Form1";
+            this.Text = "Converter String";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.Label lblParse;
+        private System.Windows.Forms.Label lblTryParse;
+        private System.Windows.Forms.Button txtLimpar;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.TextBox txtNumero2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio_1110
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// O TryParse método é como a Parse método, exceto o TryParse método gerará uma exceção se a conversão falhar. 
+        /// Ele elimina a necessidade de usar a identificação de exceções para testar um FormatException caso esse s seja 
+        /// inválido e não possa ser analisado com êxito. MSDN
+        /// </summary>
+
+        public void ConversaoParse() { 
+            try {
+                //Conversão Parse
+                int parse = Int32.Parse(txtNumero.Text);
+                lblParse.Text = ("Parse: " + parse.ToString());
+            } catch (FormatException e) { MessageBox.Show(e.Message + "\nDigite o primeiro número novamente!");
+                txtNumero.Focus();
+            }
+        }
+
+        public void ConversaoTryParse() {
+            //Conversão TryParse
+            int numero;
+            bool result = Int32.TryParse(txtNumero2.Text, out numero);
+            if (result) {
+                lblTryParse.Text = ("TryParse: " + numero.ToString());
+            }
+            else {
+                MessageBox.Show("Entrada Inválida!" + "\nDigite novamente");
+            }            
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e) {
+            this.ConversaoParse();
+            this.ConversaoTryParse();
+        }
+
+        private void txtLimpar_Click(object sender, EventArgs e) {
+            txtNumero.Text = string.Empty;
+            txtNumero2.Text = string.Empty;
+            lblParse.Text = string.Empty;
+            lblTryParse.Text = string.Empty;
+            txtNumero.Focus();
+        }
+
+        private void txtNumero_keydown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter)
+                this.txtNumero2.Focus();
+        }
+
+        private void txtNumero2_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter)
+                this.ConversaoParse();
+                this.ConversaoTryParse();
+        }
+    }
+}
